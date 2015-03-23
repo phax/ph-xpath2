c772e28b784c3cedad6bd0e6a6cc521508e55778
Philip Helger <philip@helger.com>
2015-03-23 17:48:51 +0100

    Start String escape handling

---
+++ src/main/java/com/helger/xp2/parser/XP2ParserUtils.java
@@ -0,0 +1,41 @@
+package com.helger.xp2.parser;
+
+import javax.annotation.Nonnull;
+import javax.annotation.concurrent.Immutable;
+
+import com.helger.commons.string.StringHelper;
+
+@Immutable
+public final class XP2ParserUtils
+{
+  private XP2ParserUtils ()
+  {}
+
+  @Nonnull
+  public static String getUnescapedStringDQ (@Nonnull final String s)
+  {
+    if (true)
+      return s;
+
+    final String sText = s.substring (1, s.length () - 2);
+    final int i = sText.indexOf ("\"\"");
+    if (i < 0)
+      return sText;
+
+    return StringHelper.replaceAll (sText, "\"\"", "\"");
+  }
+
+  @Nonnull
+  public static String getUnescapedStringSQ (@Nonnull final String s)
+  {
+    if (true)
+      return s;
+
+    final String sText = s.substring (1, s.length () - 2);
+    final int i = sText.indexOf ("''");
+    if (i < 0)
+      return sText;
+
+    return StringHelper.replaceAll (sText, "''", "'");
+  }
+}
